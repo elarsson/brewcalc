54561d72ad66c6694cc937ac1a87f8e1a39ff45e
Erik Larsson <elarsson@gmail.com>
2013-11-27 13:01:20 +0100

    add some transformation functions. doesnt compile now, need to fix imports and stuff

--- Ingredients.hs
+++ Ingredients.hs
@@ -3,20 +3,36 @@ module Ingredients
 , Volume
 , Fermentable(..)
 , Mash(..)
+, Wort(..)
+, Beer(..)
 ) where
 
 newtype Weight = Grams Integer
 newtype Volume = Milliliters Integer
 type Percentage = Integer
 type Efficiency = Percentage
+type Density = Float
+newtype Duration = Minutes Integer
+newtype Bitterness = IBU Integer
+type ABV = Percentage
 
 data Fermentable = Fermentable	{  name :: String,
-								   fermentableContent :: Percentage
+								   fermentableContent :: Percentage -- TODO: use to calculate efficiency
 								}
 
+data Hops = Hops { 	hopname :: String,
+					alphacontent :: Percentage
+				 }
 
-data Mash = Mash { 	fermentables :: [Fermentable -> Weight],
+data Mash = Mash { 	fermentables :: [(Fermentable, Weight)],
 					water :: Volume
 				 }
 
-data Wort = Wort Mash Volume Efficiency
+data Wort = Wort Mash Volume Density
+
+data HoppedWort = HoppedWort { 	wort :: Wort,
+								hops :: [(Hops, Duration)]
+							 }
+
+data Beer = Beer HoppedWort Bitterness ABV
+
--- Transformations.hs
+++ Transformations.hs
@@ -1,6 +1,24 @@
 module Transformations
-( temp
+( initiateMash
+, addFermentable
+, sparge
+, boil
+, ferment
 ) where
 import Ingredients
 
-temp = 0
+initiateMash :: Volume -> Mash
+initiateMash vol = Mash { fermentables = [], water = vol }
+
+addFermentable :: Fermentable -> Weight -> Mash -> Mash
+addFermentable ferm wt mash = Mash { fermentables = (ferm, wt) : fermentables mash, water = water mash }
+
+sparge :: Mash -> Volume -> Density -> Wort
+sparge mash vol density = Wort mash vol density
+
+boil :: HoppedWort -> Duration -> HoppedWort
+boil hoppedwort (Minutes mins) = 
+	hoppedwort { hops = map (\(n, Minutes oldmins) -> (n, Minutes $ oldmins + mins)) $ hops hoppedwort} -- TODO, calculate boiloff and new density
+
+ferment :: HoppedWort -> Density -> Beer
+ferment hoppedwort finalgravity = Beer hoppedwort (IBU 0) 5 -- TODO: calculate IBU from (hops,dur) and ABV from gravity
